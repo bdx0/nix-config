4de64250fabe22d41a52001a39be180131cd559f
Dương Bảo Duy <682918+bdx0@users.noreply.github.com>
2024-11-14 10:51:50 +0700

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Make flake for nix-config";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = { nixpkgs, flake-utils, ... }@inputs:
+  let
+    darwinSystems = [ "aarch64-darwin", "x86_64-darwin" ];
+    linuxSystems = [ "x86_64-linux", "aarch64-linux" ];
+    allSystems = linuxSystem ++ darwinSystems;
+  in flake-parts.lib.mkFlake (inputs // {}) {
+    debug = true;
+    flake = {};
+    systems = allSystems;
+    perSystems = {pkgs, system, ... }: {
+      devShells = {
+        default = pkgs.mkShell {
+          NIX_CONFIG = "experimental-features = nix-command flakes";
+          buildInputs = with pkgs; [
+            #
+            lsd
+            tig
+            git
+            neovim
+            emacs
+            gh
+            nixfmt-classic
+            nix
+            home-manager
+          ];
+        };
+      };
+    };
+  };
+}
